c3a313c1d7ad50162911041fa4d8f9b4e5a296ec
Irene Nguyen <irenenguyen1017@gmail.com>
2022-11-13 09:38:26 +1100

    updated env

--- .flaskenv
+++ .flaskenv
@@ -3,3 +3,5 @@ FLASK_RUN_HOST=localhost
 FLASK_RUN_PORT=3000
 FLASK_ENV=development
 FLASK_DEBUG=1
+PROD_DATABASE_URI="postgresql:///online-shop"
+DEV_DATABASE_URI="postgresql:///online-shop"
--- app/config.py
+++ app/config.py
@@ -8,7 +8,8 @@ load_dotenv(path.join(basedir, ".flaskenv"))
 
 class Config:
     PROPAGATE_EXCEPTIONS = True
-    API_TITLE = "Online shop API"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    API_TITLE = "Online store API"
     API_VERSION = "v1"
 
 
@@ -16,18 +17,18 @@ class ProdConfig(Config):
     FLASK_ENV = "production"
     DEBUG = False
     TESTING = False
-    DATABASE_URI = environ.get("PROD_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = environ.get("PROD_DATABASE_URI")
 
 
 class DevConfig(Config):
     FLASK_ENV = "development"
     DEBUG = True
     TESTING = True
-    DATABASE_URI = environ.get("DEV_DATABASE_URI")
     OPENAPI_VERSION = "3.0.3"
     OPENAPI_SWAGGER_UI_PATH = "/"
     OPENAPI_SWAGGER_UI_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.24.2/"
     OPENAPI_URL_PREFIX = "/"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///local-store.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = (
+        environ.get("DEV_DATABASE_URI") or "sqlite:///local-store.db"
+    )
     JWT_SECRET_KEY = "25432980928529878777477170733653477453"
