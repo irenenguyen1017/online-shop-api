6de29286f24ae7dba05e9f4a3068f25cbeb0db1c
Irene Nguyen <irenenguyen1017@gmail.com>
2022-11-12 23:43:42 +1100

    add get endpoint for product

--- app/resources/product.py
+++ app/resources/product.py
@@ -1,12 +1,25 @@
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.models.product import ProductModel
+from app.schemas import ProductSchema, ProductUpdateSchema
+
+NO_PRODUCT_FOUND_MESSAGE = "No product found."
 
 product_blueprint = Blueprint(
     "product", __name__, description="Operations for product."
 )
 
 
-@product_blueprint.route("/products")
-class Products(MethodView):
-    def get(self):
-        pass
+@product_blueprint.route("/product/<int:product_id>")
+class Product(MethodView):
+    @product_blueprint.response(200, ProductSchema)
+    def get(self, product_id):
+
+        product = ProductModel.find_by_id(product_id)
+
+        if product:
+            return product
+        else:
+            abort(404, message=NO_PRODUCT_FOUND_MESSAGE)
