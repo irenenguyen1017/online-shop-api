619b7f2588c17721cf98e4341ec7987324d089e0
Irene Nguyen <irenenguyen1017@gmail.com>
2022-11-12 23:58:50 +1100

    add jwt config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 
+from app.blocklist import BLOCKLIST
 from app.database import db
 from app.resources.comment import comment_blueprint
 from app.resources.product import product_blueprint
@@ -14,6 +16,40 @@ def create_app() -> Flask:
     app.config.from_object("app.config.DevConfig")
     db.init_app(app)
     api = Api(app)
+
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_loader(jwt_payload):
+        return jsonify(
+            {"message": "The token has been revoked.", "error": "token_revoked"}, 401
+        )
+
+    @jwt.expired_token_loader
+    def expired_token_loader_handler(jwt_header, jwt_payload):
+        return jsonify(
+            {"message": "Expired token provided.", "error": "token_expired"}, 401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_handler(jwt_payload):
+        return jsonify(
+            {"message": "Invalid token provided.", "error": "token_invalid"}, 401
+        )
+
+    @jwt.unauthorized_loader
+    def unauthorized_loader(jwt_payload):
+        return jsonify(
+            {
+                "message": "Request does not contain an access token.",
+                "error": "token_expired",
+            },
+            401,
+        )
 
     @app.before_first_request
     def create_tables():
